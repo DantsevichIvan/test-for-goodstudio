328fe942fd525ef580640d92ed8d6d7aa88c50f9
DantsevichIvan <iw.dantsevich@gmail.com>
2020-07-28 16:55:28 +0300

    Updates

---
+++ precache-manifest.4d928dac7d183a246c27489540356548.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b408635bb8482534caa652fb377210ff",
+    "url": "/test-for-goodstudio/index.html"
+  },
+  {
+    "revision": "afce3bad5239a706407e",
+    "url": "/test-for-goodstudio/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "9a41c3205543204398ed",
+    "url": "/test-for-goodstudio/static/css/main.5ce9e6e2.chunk.css"
+  },
+  {
+    "revision": "afce3bad5239a706407e",
+    "url": "/test-for-goodstudio/static/js/2.5e829d5d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test-for-goodstudio/static/js/2.5e829d5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a41c3205543204398ed",
+    "url": "/test-for-goodstudio/static/js/main.8d1ddbbc.chunk.js"
+  },
+  {
+    "revision": "e107b235b79e5c531739",
+    "url": "/test-for-goodstudio/static/js/runtime-main.db029f42.js"
+  }
+]);
